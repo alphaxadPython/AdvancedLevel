9d724e1e34dd63f97e5db8b21651d2f0f74781bf
alphaxadPython <alphaxadcoder@gmail.com>
2021-07-01 18:33:32 +0300

    all users

--- AdvancedLevel/src/advancedlevel/DashboardController.java
+++ AdvancedLevel/src/advancedlevel/DashboardController.java
@@ -3,10 +3,26 @@ package advancedlevel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 
 public class DashboardController implements Initializable {
+
+    @FXML
+    private Button profile5;
+    @FXML
+    private Button profile6;
+    
+    @FXML
+    private Button LinkForm5;
+    @FXML
+    private Button linkForm6;
+    @FXML
+    private TabPane mainTab;
     
    
     
@@ -14,5 +30,43 @@ public class DashboardController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void combiDetail(MouseEvent event) {
+        mainTab.getSelectionModel().select(8);
+    }
+
+   
+    @FXML
+    private void AddStaff(MouseEvent event) {
+        mainTab.getSelectionModel().select(7);
+
+    }
+
+    @FXML
+    private void formFiveProfile(MouseEvent event) {
+        mainTab.getSelectionModel().select(4);
+
+    }
+
+    @FXML
+    private void AddStudent(MouseEvent event) {
+        mainTab.getSelectionModel().select(6);
+
+    }
+
+    @FXML
+    private void formSixProfile(MouseEvent event) {
+    }
+
+    
+
+    @FXML
+    private void viewForm5(MouseEvent event) {
+    }
+
+    @FXML
+    private void viewForm6(MouseEvent event) {
+    }
     
 }
---
+++ AdvancedLevel/src/advancedlevel/LoginController.java
@@ -0,0 +1,16 @@
+
+package advancedlevel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
